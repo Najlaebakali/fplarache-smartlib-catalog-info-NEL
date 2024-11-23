b067832db404ceffc52b4286959a187052f54c76
Najlaebk <bakalinajlae83@gmail.com>
2024-11-23 16:08:37 +0100

    des corrections were needed !!!

--- output.tf
+++ output.tf
@@ -1,6 +1,9 @@
 output "ecr_repository_url" {
-    value = aws_ecr_repository.app_repo.repository_url
+    value = aws_ecr_repository.catalog_repo.repository_url
 }
 output "ecs_cluster_id" {
-    value = aws_ecs_cluster.app_cluster.id
+    value = aws_ecs_cluster.catalog_cluster.id
+}
+output "ecs_service_name" {
+    value = aws_ecs_service.catalog_service.name
 }
